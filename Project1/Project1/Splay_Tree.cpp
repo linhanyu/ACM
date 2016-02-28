be7c1d0760e4527df61d2977f091468fd387751b
Dan Ge <305991071@qq.com>
2016-02-28 09:32:20 +0800

    hahw

--- Project1/Project1/Splay_Tree.cpp
+++ Project1/Project1/Splay_Tree.cpp
@@ -3,11 +3,11 @@ class Splay_Tree_Node
 {
 public:
 	T val;
-	splay_tree_node * left;
-	splay_tree_node * right;
+	Splay_Tree_Node<T> * left;
+	Splay_Tree_Node<	T> * right;
 };
 
-template <template T>
+template <class T>
 class Splay_Tree
 {
 public:
@@ -15,18 +15,47 @@ public:
 	~Splay_Tree();
 
 private:
-	Splay_Tree_Node * null_node;
-	Splay_Tree_Node * header;
-	Splay_Tree_Node * tree;
+	Splay_Tree_Node<T> * null_node;
+	Splay_Tree_Node<T> * tree;
+
+	Splay_Tree_Node<T> * Splay(T val, Splay_Tree_Node<T> * pos);
 };
 
-
-template<template T>
-Splay_Tree<T>::Splay_Tree()
+template <class T>
+Splay_Tree_Node<T> * Splay_Tree<T>::Splay(T val, Splay_Tree_Node<T> * pos)
 {
-	int b;
-	null_node = new Splay_Tree_Node;
-	null_node->left = null_node->right = null_node;
+	static Splay_Tree_NodeM<T> Header;
+	Splay_Tree_Node<T> * left_tree_max;
+	Splay_Tree_Node<T> * right_tree_min;
+
+	Header.left = Header.right = null_node;
+	left_tree_max = right_tree_min = &Header;
+	null_node->val = val;
+
+	while (val != pos->val)
+	{
+		if (val < pos->val)
+		{
+			if (val < pos->left->val)
+			{
+				// 				pos = single_rotation_left(pos);
+			}
+
+			if (pos->left == null_node)
+			{
+				break;
+			}
+
+			right_tree_min->left = pos;
+			right_tree_min = pos;
+			pos = pos->left;
+		}
+	}
 }
 
-
+template <class T>
+Splay_Tree<T>::Splay_Tree()
+{
+	null_node = new Splay_Tree_Node<T>;
+	null_node->left = null_node->right = null_node;
+}

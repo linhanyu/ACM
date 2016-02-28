9160b729b8694c2286c2dbfa54536aa5a10cbb4f
Dan Ge <305991071@qq.com>
2016-02-28 08:50:12 +0800

    hehe

---
+++ Project1/Project1/Splay_Tree.cpp
@@ -0,0 +1,31 @@
+template<class T>
+class Splay_Tree_Node
+{
+public:
+	T val;
+	splay_tree_node * left;
+	splay_tree_node * right;
+};
+
+template <template T>
+class Splay_Tree
+{
+public:
+	Splay_Tree();
+	~Splay_Tree();
+
+private:
+	Splay_Tree_Node * null_node;
+	Splay_Tree_Node * header;
+	Splay_Tree_Node * tree;
+};
+
+
+template<template T>
+Splay_Tree<T>::Splay_Tree()
+{
+	null_node = new Splay_Tree_Node;
+	null_node->left = null_node->right = null_node;
+}
+
+
